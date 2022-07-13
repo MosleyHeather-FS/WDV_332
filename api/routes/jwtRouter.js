01d865c1e1fff068b2f60bb58a48782b2b3fddac
Heather Mosley <hmosley@student.fullsail.edu>
2022-07-13 15:58:07 -0500

    Added Token to Profile and Login

--- api/routes/jwtRouter.js
+++ api/routes/jwtRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
+const checkAuth = require('../../auth/check.Auth')
 require('dotenv').config();
 
 router.use(express.json());
@@ -9,23 +10,13 @@ router.use(express.json());
 router.post('/login', (req, res, next) => {
     const email = req.body.email;
     const id = req.body.id;
-    const token = jwt.sign({ email: email, id: id }, process.env.JWTKEY,)
+    const token = jwt.sign({ email: email, id: id }, process.env.JWTKEY, { expiresIn: '30m', })
 
     res.status(200).json({ message: 'Secure', token: token })
 });
 
-router.get('/profile', (req, res, next) => {
-    // header - Authorization: Bearer (secret key)
-    // Placed in a try to catch errors, can be placed by itself without the error, but always best to do error catches where possible
-   try{
-    const token = req.headers.authorization.split(' ')[1];
-    const decoded = jwt.verify(token, process.env.JWTKEY);
-
-    res.status(200).json({ message: 'Authorization Verified', decoded: decoded })
-   }
-   catch(err){
-       res.status(401).json({ message: 'Authorization Failed' })
-   }
+router.get('/profile', checkAuth, (req, res, next) => {
+    res.status(200).json({ message: decoded })
 });
 
 module.exports = router;
